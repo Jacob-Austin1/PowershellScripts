02ee8fedc9c28a59d14d60a2a3c6ec0b0a98ffa1
Jacob-Austin1 <95717500+Jacob-Austin1@users.noreply.github.com>
2024-10-30 16:11:10 -0400

    Added PS script fetching

--- AD-PV-setup.ps1
+++ AD-PV-setup.ps1
@@ -1,8 +1,10 @@
 New-Item -ItemType Directory -Path "C:\Users\robb.stark\Desktop\Practical" -Force
 $AD = "C:\Users\robb.stark\Desktop\Practical\ADModule.zip"
 $PV = "C:\Users\robb.stark\Desktop\Practical\PowerView.ps1"
+$PSF = "C:\Users\robb.stark\Desktop\PSScripts"
 iwr 192.168.56.5:8080/ADModule.zip -Outfile $AD
 iwr 192.168.56.5:8080/PowerView -OutFile $PV
+iwr 192.168.56.5:8080/PSScripts -OutFile $PSF
 Unblock-File $AD
 Expand-Archive $AD
 Unblock-File .\ADModule\ADModule-master\Microsoft.ActiveDirectory.Management.dll
