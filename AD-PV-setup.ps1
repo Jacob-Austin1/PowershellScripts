eb4bfe673cd0579f24f8a692e4757ba11dc92419
Jacob-Austin1 <95717500+Jacob-Austin1@users.noreply.github.com>
2024-10-31 18:32:45 -0400

    Update AD-PV-setup.ps1

--- AD-PV-setup.ps1
+++ AD-PV-setup.ps1
@@ -1,11 +1,13 @@
 New-Item -ItemType Directory -Path "C:\Users\robb.stark\Desktop\Practical" -Force
 $AD = "C:\Users\robb.stark\Desktop\Practical\ADModule.zip"
 $PV = "C:\Users\robb.stark\Desktop\Practical\PowerView.ps1"
-$PSF = "C:\Users\robb.stark\Desktop\PSScripts"
+$ADDDPP = "C:\Users\robb.stark\Desktop\admin-run-adddpp.ps1"
+$DSPA =  "C:\Users\robb.stark\Desktop\admin-run-dspa.ps1"
 $WINDAP = "C:\Users\robb.stark\Desktop\windapsearch-windows-amd64.exe"
 iwr 192.168.56.5:8080/ADModule.zip -Outfile $AD
 iwr 192.168.56.5:8080/PowerView -OutFile $PV
-iwr 192.168.56.5:8080/PSScripts -OutFile $PSF
+iwr 192.168.56.5:8080/admin-run-dspa -OutFile $DSPA
+iwr 192.168.56.5:8080/admin-run-adddpp -OutFile $ADDDPP
 iwr 192.168.56.5:8080/windapsearch-windows-amd64.exe $WINDAP
 Unblock-File $AD
 Expand-Archive $AD
